980b84274232fecce12a1a8188bde72ebfd2f1cf
rabbit1031 <rabbit1031@users.noreply.github.com>
2019-04-20 14:34:36 +0900

    fix makelink.sh

--- makelink.sh
+++ makelink.sh
@@ -32,16 +32,15 @@ function link_zshfiles() {
 
 function link_vimfiles() {
   ln ${ln_options[*]} ${DOTFILES_DIR}/.vimrc ${HOME}/.vimrc
-  if [ -d ${HOME}/.vim/colors ]; then
-    mkdir -pv ${HOME}/.vim/colors
-  fi
 
-  test -d ${HOME}/.config/nvim && mkdir -pv ${HOME}/.config/nvim
+  ! test -d ${HOME}/.config/nvim && mkdir -pv ${HOME}/.config/nvim
   ln ${ln_options[*]} ${DOTFILES_DIR}/vim/init.vim ${HOME}/.config/nvim/init.vim
   ln ${ln_options[*]} ${DOTFILES_DIR}/vim/plugins.toml ${HOME}/.config/nvim/plugins.toml
 }
 
 function link_atomfiles() {
+  ! test -d ${HOME}/.atom && mkdir -pv ${HOME}/.atom
+
   ln ${ln_options[*]} ${DOTFILES_DIR}/atom/config.cson ${HOME}/.atom/config.cson
   ln ${ln_options[*]} ${DOTFILES_DIR}/atom/github.cson ${HOME}/.atom/github.cson
   ln ${ln_options[*]} ${DOTFILES_DIR}/atom/init.coffee ${HOME}/.atom/init.coffee
@@ -53,4 +52,4 @@ function link_atomfiles() {
 link_bashfiles
 link_zshfiles
 link_vimfiles
-link_atomfiles
+# link_atomfiles
